3094673834576d75a33297ade150e69681e1806d
Jung.Sohn <91623332+jrson11@users.noreply.github.com>
2023-10-29 21:17:39 -0500

    Update GeoSohn_20231028.py

--- streamlit/GeoSohn_20231028.py
+++ streamlit/GeoSohn_20231028.py
@@ -6,13 +6,14 @@ st.markdown('# GeoSohn Apps by Jung')
 ## Sidebar
 password = st.sidebar.text_input('Password?', 'password')
 
+'''
 if password == st.secrets['db_password']:
   #st.text('Ask Jung for the pw')
   selected_app = st.sidebar.selectbox("Select an App", ['Mudmat_Moments','TBD'])
 else:
   #st.text('This is for only bp teams')
   selected_app = st.sidebar.selectbox("Select an App", ['Mudmat_Moments','GoM_Kaskida'])
-
+'''
 
 '''
 # Main --------------------------------------------------
