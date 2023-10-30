5b6bf714a164ec53017878b63aeea640287877ff
Jung.Sohn <91623332+jrson11@users.noreply.github.com>
2023-10-29 21:24:16 -0500

    Update GeoSohn_20231028.py

--- streamlit/GeoSohn_20231028.py
+++ streamlit/GeoSohn_20231028.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-# Initialization ----------------------------------------
-st.markdown('# GeoSohn Apps by Jung')
+# =======================================================
+# Initialization 
+# =======================================================
 
-## Sidebar
+## Sidebar ----------------------------------------------
 password = st.sidebar.text_input('Password?', 'password')
 
 #if password == st.secrets['db_password']:
@@ -14,5 +15,17 @@ else:
   st.sidebar.text('Ask Jung for the pw')
   selected_app = st.sidebar.selectbox("Select an App", ['Mudmat_Moments','TBD'])
 
+## Main --------------------------------------------------
+st.markdown('# GeoSohn Apps by Jung')
+
+  
+# Display two columns
+col1, col2 = st.columns(2)
+#
+with col1:
+  st.text('col1')
+with col2:
+  st.text('col2')
+  tab0,tab1,tab2,tab3,tab4,tab5 = st.tabs(['A.Map','B.Plasticity','C.Field.Su',"3.$S_u/σ'_{vc}$",'4.Norm.DSS','5.Est.DSS'])
 
 
