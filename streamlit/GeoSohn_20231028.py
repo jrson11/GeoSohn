e7e048fb502b4ac2ce0c16f0d98f4f7fbc0e78a1
Jung.Sohn <91623332+jrson11@users.noreply.github.com>
2023-10-29 21:20:03 -0500

    Update GeoSohn_20231028.py

--- streamlit/GeoSohn_20231028.py
+++ streamlit/GeoSohn_20231028.py
@@ -6,25 +6,13 @@ st.markdown('# GeoSohn Apps by Jung')
 ## Sidebar
 password = st.sidebar.text_input('Password?', 'password')
 
-
+#if password == st.secrets['db_password']:
 if password == 'bpbp':
-  #st.text('Ask Jung for the pw')
+  st.text('Ask Jung for the pw')
   selected_app = st.sidebar.selectbox("Select an App", ['Mudmat_Moments','TBD'])
 else:
-  #st.text('This is for only bp teams')
+  st.text('This is for only bp teams')
   selected_app = st.sidebar.selectbox("Select an App", ['Mudmat_Moments','GoM_Kaskida'])
 
 
-'''
-# Main --------------------------------------------------
-def main():
-  st.write('Assa')
 
-# Membership --------------------------------------------
-if password == st.secrets['db_password']:
-  st.markdown('## Welcome to KUPEA')
-  main()
-  
-else:
-  st.markdown('## Please join KUPEA')
-'''
