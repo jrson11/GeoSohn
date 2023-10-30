5721af5568e2010cec4acb82b1273eff2eb1392d
Jung.Sohn <91623332+jrson11@users.noreply.github.com>
2023-10-29 21:21:17 -0500

    Update GeoSohn_20231028.py

--- streamlit/GeoSohn_20231028.py
+++ streamlit/GeoSohn_20231028.py
@@ -8,11 +8,11 @@ password = st.sidebar.text_input('Password?', 'password')
 
 #if password == st.secrets['db_password']:
 if password == 'bpbp':
-  st.text('Ask Jung for the pw')
-  selected_app = st.sidebar.selectbox("Select an App", ['Mudmat_Moments','TBD'])
-else:
-  st.text('This is for only bp teams')
+  st.sidebar.text('This is for only bp teams')
   selected_app = st.sidebar.selectbox("Select an App", ['Mudmat_Moments','GoM_Kaskida'])
+else:
+  st.sidebar.text('Ask Jung for the pw')
+  selected_app = st.sidebar.selectbox("Select an App", ['Mudmat_Moments','TBD'])
 
 
 
