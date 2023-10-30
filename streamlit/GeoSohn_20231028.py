2bc340699ca7411b74955440cddbbe18945a8bee
Jung.Sohn <91623332+jrson11@users.noreply.github.com>
2023-10-29 21:13:28 -0500

    Update GeoSohn_20231028.py

--- streamlit/GeoSohn_20231028.py
+++ streamlit/GeoSohn_20231028.py
@@ -3,6 +3,10 @@ import streamlit as st
 # Initialization ----------------------------------------
 st.markdown('# GeoSohn Apps by Jung')
 password = st.sidebar.text_input('Password?', 'password')
+
+selected_app = st.sidebar.selectbox("Select an App", ['Mudmat_Moments','TBD'])
+
+
 '''
 # Main --------------------------------------------------
 def main():
