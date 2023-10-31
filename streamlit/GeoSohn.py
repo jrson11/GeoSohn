7d9bffff72ed3997bb9579712f30738e8598835e
Jung.Sohn <91623332+jrson11@users.noreply.github.com>
2023-10-30 20:17:33 -0500

    Update GeoSohn.py

--- streamlit/GeoSohn.py
+++ streamlit/GeoSohn.py
@@ -20,6 +20,18 @@ else:
 ## Main --------------------------------------------------
 st.markdown('# GeoSohn Apps by Jung')
 
+def moment_disp():
+  # Display two columns
+  col1, col2 = st.columns(2)
+  #
+  with col1:
+    st.text('Plots')
+  
+  with col2:
+    st.text('Data')
+    #tab0,tab1,tab2,tab3,tab4 = st.tabs(['1-Structural_Coordinates','2-Structural_Loads','3-','4-','5-'])
+
+
 if selected_app == 'Mudmat_Moments':
   st.text('mudmat')
   mm.mudmat_disp
