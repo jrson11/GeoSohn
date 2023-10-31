ca582c874519edb8d972926cbe47a22e4ce1fcc6
Jung.Sohn <91623332+jrson11@users.noreply.github.com>
2023-10-30 20:11:27 -0500

    Update mudmat_moments.py

--- streamlit/mudmat_moments.py
+++ streamlit/mudmat_moments.py
@@ -1,4 +1,14 @@
 import numpy as np
 import pandas as pd
 
-
+# Display two columns
+col1, col2 = st.columns(2)
+#
+with col1:
+  st.text('Plots')
+  #plot_topview()
+  #plot_fontview()
+  
+with col2:
+  st.text('Data')
+  tab0,tab1,tab2,tab3,tab4 = st.tabs(['1-Structural_Coordinates','2-Structural_Loads','3-','4-','5-'])
