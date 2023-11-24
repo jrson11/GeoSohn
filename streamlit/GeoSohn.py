962fa208b3ffb83675bc356b339212059d2081bc
Jung.Sohn <91623332+jrson11@users.noreply.github.com>
2023-11-24 10:28:53 -0600

    Update GeoSohn.py

--- streamlit/GeoSohn.py
+++ streamlit/GeoSohn.py
@@ -9,13 +9,7 @@ def main():
     st.title("Main Page")
     st.markdown("## Welcome to the Main Page")
 
-    # Link to App 1
-    if st.button("Go to App 1"):
-        st.markdown("[Click here](http://localhost:8501/app1) to go to App 1")
 
-    # Link to App 2
-    if st.button("Go to App 2"):
-        st.markdown("[Click here](http://localhost:8501/app2) to go to App 2")
 
 if __name__ == "__main__":
     main()
