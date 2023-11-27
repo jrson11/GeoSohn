e4a3bbbba154ca5495e0785132622712cf6ca5f7
Jung.Sohn <91623332+jrson11@users.noreply.github.com>
2023-11-27 14:03:33 -0600

    Update GeoSohn.py

--- streamlit/GeoSohn.py
+++ streamlit/GeoSohn.py
@@ -1,15 +1,51 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import altair as alt
 
-def main():
+## Setup main streamlit options
+#st.set_page_config(layout="wide") # wide / centered 
 
-    # Sidebar
-    password = st.sidebar.text_input('Password?', '******')
+
+# ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+# Functions
+
+class ImportData:
+    def __init__(self, selected_project):
+        self.selected_project = selected_project
+
+class SiteInvestigation:
+    def __init__(self, selected_project):
+        self.selected_project = selected_project
+
+class SoilLabTesting:
+    def __init__(self, selected_project):
+        self.selected_project = selected_project
+
+class ShallowFoundation:
+    def __init__(self, selected_project):
+        self.selected_project = selected_project
     
-    # Main Page
-    st.title("Main Page")
-    st.markdown("## Welcome to the Main Page")
+# ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+# Main
 
 
+# Initialization
+selected_analysis = st.selectbox("Select Analysis", ["Digitized Data", "Site Investigation", "Soil Lab Testing", "Shallow Foundation", "Deep Foundation", "Risk Assessment"])
 
-if __name__ == "__main__":
-    main()
+# Sidebar
+pw = st.sidebar.text_input('Password = ', '?')
+if pw == st.secrets['DB_pw']:
+  selected_project = st.sidebar.selectbox("Select Project", ["None", "Kaskida", "Argos", "NaKika"])
+else:
+  selected_project = st.sidebar.selectbox("Select Project", ["None"])
+
+# Main
+if selected_analysis == 'Digitized Data':
+    data = ImportData(selected_project)
+elif selected_analysis == 'Site Investigation':
+    site_investigation = SiteInvestigation(selected_project)
+elif selected_analysis == 'Soil Lab Testing':
+    soil_lab_testing = Soil Lab Testing(selected_project)
+elif selected_analysis == 'Shallow Foundation':
+    shallow_foundation = ShallowFoundation(selected_project)
