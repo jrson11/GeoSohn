8a5b933ea0b4fbb9a26613c7d70bd0a2c8234158
Jung.Sohn <91623332+jrson11@users.noreply.github.com>
2023-11-27 13:43:19 -0600

    Update streamlit_geosohn_public.py

--- docs/streamlit_public/streamlit_geosohn_public.py
+++ docs/streamlit_public/streamlit_geosohn_public.py
@@ -1,7 +1,7 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import altair as alt
+#import pandas as pd
+#import matplotlib.pyplot as plt
+#import altair as alt
 
 ## Setup main streamlit options
-st.set_page_config(layout="wide") # wide / centered 
+#st.set_page_config(layout="wide") # wide / centered 
