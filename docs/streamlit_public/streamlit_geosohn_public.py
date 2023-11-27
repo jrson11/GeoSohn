6280094d0f0ce491cd4b7ceed0603af3fda5ea22
Jung.Sohn <91623332+jrson11@users.noreply.github.com>
2023-11-27 13:46:53 -0600

    Update streamlit_geosohn_public.py

--- docs/streamlit_public/streamlit_geosohn_public.py
+++ docs/streamlit_public/streamlit_geosohn_public.py
@@ -5,3 +5,13 @@ import altair as alt
 
 ## Setup main streamlit options
 #st.set_page_config(layout="wide") # wide / centered 
+
+
+# ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+# Main
+
+# Sidebar
+selected_project = st.sidebar.selectbox("Select Project", ["None", "Kaskida", "Argos", "NaKika"])
+
+# Main content
+selected_option = st.selectbox("Select Option", ["Data", "Site Investigation", "Soil Lab Testing", "Foundation Design"])
