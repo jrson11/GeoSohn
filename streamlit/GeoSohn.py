96bc882ece30a74193ed1dc5faa6eb1632bca271
Jung.Sohn <91623332+jrson11@users.noreply.github.com>
2023-11-27 14:57:55 -0600

    Update GeoSohn.py

--- streamlit/GeoSohn.py
+++ streamlit/GeoSohn.py
@@ -21,7 +21,6 @@ class Data:
         file_path = f"https://github.com/jrson11/GeoSohn/edit/main/streamlit/db_csv/"
         file_K = "AGS_Kaskida_CSV.csv"
         df_K = pd.read_csv(file_path+file_K)
-
         # Check
         st.dataframe(df_K)
 
