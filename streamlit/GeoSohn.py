8bb2f2f4d28ef42af22203e5333ee1b904784a3e
Jung.Sohn <91623332+jrson11@users.noreply.github.com>
2023-11-28 19:19:25 -0600

    Update GeoSohn.py

--- streamlit/GeoSohn.py
+++ streamlit/GeoSohn.py
@@ -70,27 +70,3 @@ st.image("https://raw.githubusercontent.com/jrson11/GeoSohn/main/docs/images/Can
 # F. Plot
 
 
-
-'''
-# Initialization
-selected_analysis = st.selectbox("Select Analysis", ["Digitized Data", "Site Investigation", "Soil Lab Testing", "Shallow Foundation", "Deep Foundation", "Risk Assessment"])
-st.image("https://raw.githubusercontent.com/jrson11/GeoSohn/main/docs/images/Canvas_of_Offshore_Geotech(Sep2023).png")
-
-# Sidebar
-pw_input = st.sidebar.text_input('Password = ', '?')
-if pw_input == st.secrets['DB_pw']:
-  selected_project = st.sidebar.selectbox("Select Project", ["All", "Kaskida", "Argos", "NaKika"])
-else:
-  selected_project = st.sidebar.selectbox("Select Project", ["None"])
-
-# Main
-if selected_analysis == 'Digitized Data':
-    data = Data(selected_project)
-    data.run()
-elif selected_analysis == 'Site Investigation':
-    site_investigation = SiteInvestigation(selected_project)
-elif selected_analysis == 'Soil Lab Testing':
-    soil_lab_testing = SoilLabTesting(selected_project)
-elif selected_analysis == 'Shallow Foundation':
-    shallow_foundation = ShallowFoundation(selected_project)
-'''
