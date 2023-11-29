3125e76dcb7e37f5dc8a40177acd6be5d231e1cc
Jung.Sohn <91623332+jrson11@users.noreply.github.com>
2023-11-28 19:14:44 -0600

    Update GeoSohn.py

--- streamlit/GeoSohn.py
+++ streamlit/GeoSohn.py
@@ -29,8 +29,17 @@ import matplotlib.pyplot as plt
 
 # ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
 # A. Functions
-class A():
-    a = 10
+class ImportData:
+    def __init__(self, file):
+        self.PROJ = pd.read_excel(file, sheet_name="PROJ", header=2)
+        self.LOCA = pd.read_excel(file, sheet_name="LOCA", header=2)
+        self.GEOL = pd.read_excel(file, sheet_name="GEOL", header=2)
+        self.SCPT = pd.read_excel(file, sheet_name="SCPT", header=2)
+        
+## Project Kaskida
+df_Kaskida = ImportData("./src_AGS/AGS_Kaskida(24Nov23).xlsx")
+df_NaKika = ImportData("./src_AGS/AGS_NaKika(24Nov23).xlsx")
+df_Argos = ImportData("./src_AGS/AGS_Argos(24Nov23).xlsx")
 
 # ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
 # B. Sidebar
@@ -43,8 +52,13 @@ else:
 # ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
 # C. Main
 
+## Intro
 selected_analysis = st.selectbox("Select Analysis", ["Data", "Site Investigation", "Lab Testing", "Shallow Foundation", "Deep Foundation", "Risk Assessment"])
 st.image("https://raw.githubusercontent.com/jrson11/GeoSohn/main/docs/images/Canvas_of_Offshore_Geotech(Sep2023).png")
+
+## Analysis
+#if selected_analysis = 'Data':
+#    data = DATA(selected_project)
 
 # ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
 # D. Calculation
