cabea949002590f1aff1d879da52d29210934eba
Jung.Sohn <91623332+jrson11@users.noreply.github.com>
2023-11-28 19:41:42 -0600

    Update GeoSohn.py

--- streamlit/GeoSohn.py
+++ streamlit/GeoSohn.py
@@ -34,33 +34,53 @@ class ImportData:
         self.SCPT = pd.read_excel(file, sheet_name="SCPT", header=2)
 
 # Note: Use the raw file URL on GitHub to get the correct file content
-url_Kaskida = "https://github.com/jrson11/GeoSohn/raw/main/streamlit/src_AGS/AGS_Kaskida(24Nov23).xlsx"
+#url_Kaskida = "https://github.com/jrson11/GeoSohn/raw/main/streamlit/src_AGS/AGS_Kaskida(24Nov23).xlsx"
+url_Kaskida = "./src_AGS/AGS_Kaskida(24Nov23).xlsx"
 df_Kaskida = ImportData(url_Kaskida)
+#url_Argos = "https://github.com/jrson11/GeoSohn/raw/main/streamlit/src_AGS/AGS_Argos(24Nov23).xlsx"
+url_Argos = "./src_AGS/AGS_Argos(24Nov23).xlsx"
+df_Argos = ImportData(url_Argos)
+#url_NaKika = "https://github.com/jrson11/GeoSohn/raw/main/streamlit/src_AGS/AGS_NaKika(24Nov23).xlsx"
+url_NaKika = "./src_AGS/AGS_NaKika(24Nov23).xlsx"
+df_NaKika = ImportData(url_NaKika)
 
-## Project Kaskida
-#df_Kaskida = ImportData("https://github.com/jrson11/GeoSohn/blob/main/streamlit/src_AGS/AGS_Kaskida(24Nov23).xlsx")
-#df_NaKika = ImportData("./src_AGS/AGS_NaKika(24Nov23).xlsx")
-#df_Argos = ImportData("./src_AGS/AGS_Argos(24Nov23).xlsx")
+# Create a dictionary to store data frames based on project names
+project_data = {
+    'All': None,
+    'Kaskida': df_Kaskida,
+    'Argos': df_Argos,
+    'NaKika': df_NaKika
+}
 
 # ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
 # B. Sidebar
-pw_input = st.sidebar.text_input('Password = ', '?')
-if pw_input == st.secrets['DB_pw']:
-  selected_project = st.sidebar.selectbox("Select Project", ["All", "Kaskida", "Argos", "NaKika"])
-else:
-  selected_project = st.sidebar.selectbox("Select Project", ["None"])
-    
+# pw_input = st.sidebar.text_input('Password = ', '?')
+# if pw_input == st.secrets['DB_pw']:
+#     selected_project = st.sidebar.selectbox("Select Project", ["All", "Kaskida", "Argos", "NaKika"])
+# else:
+#     selected_project = st.sidebar.selectbox("Select Project", ["None"])
+
+selected_project = st.sidebar.selectbox("Select Project", ["All", "Kaskida", "Argos", "NaKika"])
+
 # ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
 # C. Main
 
 ## Intro
 selected_analysis = st.selectbox("Select Analysis", ["AGS Data", "Site Investigation", "Lab Testing", "Shallow Foundation", "Deep Foundation", "Risk Assessment"])
-st.image("https://raw.githubusercontent.com/jrson11/GeoSohn/main/docs/images/Canvas_of_Offshore_Geotech(Sep2023).png")
 
-## Analysis
-if selected_analysis == 'AGS Data':
-    st.dataframe(df_Kaskida.PROJ)
+# Display the image for 'All' projects
+if selected_project == 'All':
+    st.image("https://raw.githubusercontent.com/jrson11/GeoSohn/main/docs/images/Canvas_of_Offshore_Geotech(Sep2023).png")
 
+# Check if the selected project is in the dictionary
+elif selected_project in project_data:
 
+    ## Analysis
+    selected_df = project_data[selected_project]
 
+    if selected_df:
+        if selected_analysis == 'AGS Data':
+            st.dataframe(selected_df.PROJ)
 
+        elif selected_analysis == 'Site Investigation':
+            st.dataframe(selected_df.SCPT)
