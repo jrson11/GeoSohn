86bf1d664d3540db0114c884469a2f9967c36235
Jungrak.Son <91623332+jrson11@users.noreply.github.com>
2024-01-06 22:11:31 -0600

    Create sidebar_links.py

---
+++ streamlit/sidebar_links.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def add_sidebar_links():
+    st.sidebar.title("Useful Links")
+    st.sidebar.markdown("[Link A](https://example.com/A)")
+    st.sidebar.markdown("[Link B](https://example.com/B)")
+    st.sidebar.markdown("[Link C](https://example.com/C)")
