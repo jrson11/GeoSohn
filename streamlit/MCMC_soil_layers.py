1d5d8588d926747f0e18025adace06d6dfbc4d42
Jungrak.Son <91623332+jrson11@users.noreply.github.com>
2024-01-06 20:50:24 -0600

    Update MCMC_soil_layers.py

--- streamlit/MCMC_soil_layers.py
+++ streamlit/MCMC_soil_layers.py
@@ -6,6 +6,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches  
 
 # =============================================================================
 # Import raw data
