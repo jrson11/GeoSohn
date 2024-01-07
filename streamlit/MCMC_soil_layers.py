83f1fce66fb404981935844e37be76f04d271cc6
Jungrak.Son <91623332+jrson11@users.noreply.github.com>
2024-01-06 20:40:27 -0600

    Update MCMC_soil_layers.py

--- streamlit/MCMC_soil_layers.py
+++ streamlit/MCMC_soil_layers.py
@@ -1,1 +1,15 @@
+# Name: MCMC
+# Authour: Jung.Sohn
+# Date: 05Jan24
 
+import numpy as np  
+import pandas as pd  
+import matplotlib.pyplot as plt  
+import matplotlib.patches as patches  
+import streamlit as st
+
+# =============================================================================
+# Import raw data
+df_Raw = pd.read_csv('https://github.com/jrson11/GeoSohn/blob/main/streamlit/input_MCMC_soil_layers/UW_PCPT_Robertson2010.csv')  
+
+st.dataframe(df_Raw)
