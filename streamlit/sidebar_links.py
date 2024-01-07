c46dc79deb061c0e15f652815ecce0cade1cb747
Jungrak.Son <91623332+jrson11@users.noreply.github.com>
2024-01-06 22:16:46 -0600

    Update sidebar_links.py

--- streamlit/sidebar_links.py
+++ streamlit/sidebar_links.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 def add_sidebar_links():
-    st.sidebar.title("Useful Links")
-    st.sidebar.markdown("[Link A](https://example.com/A)")
-    st.sidebar.markdown("[Link B](https://example.com/B)")
-    st.sidebar.markdown("[Link C](https://example.com/C)")
+    st.sidebar.title("Links of GeoSohn Plots")
+    st.sidebar.markdown("[p-y curve](https://plotpycurvepy-j833amvwazrskraswjxgbw.streamlit.app/)")
+    st.sidebar.markdown("[q-z curve](https://plotqzcurvepy-2kegu9kpatcrcpwgn3vfas.streamlit.app/)")
+    st.sidebar.markdown("[t-z curve](https://plottzcurvepy-mgjf64hfh4dbueqt8rcoez.streamlit.app/)")
+    st.sidebar.markdown("[shallow foundation](https://plotshallowfoundationpy-pcmnvaiggvlpbsdqzzs4bo.streamlit.app/)")
