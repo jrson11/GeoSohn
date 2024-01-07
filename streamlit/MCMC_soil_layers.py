e9e5e8d81dd4d5f0c4803cd49b70b4981d96f230
Jungrak.Son <91623332+jrson11@users.noreply.github.com>
2024-01-06 22:24:35 -0600

    Update MCMC_soil_layers.py

--- streamlit/MCMC_soil_layers.py
+++ streamlit/MCMC_soil_layers.py
@@ -12,7 +12,7 @@ import matplotlib.patches as patches
 
 # =============================================================================
 # Import raw data
-df_Raw = pd.read_csv('https://raw.github.com/jrson11/GeoSohn/blob/main/streamlit/input_MCMC_soil_layers/UW_PCPT_Robertson2010.csv')  
+df_Raw = pd.read_csv('https://raw.githubusercontent.com/jrson11/GeoSohn/blob/main/streamlit/input_MCMC_soil_layers/UW_PCPT_Robertson2010.csv')  
 cols = df_Raw.columns   # DataFrame show have two columns, i.e., Z and X
 z_header = cols[0]
 x_header = cols[1]
