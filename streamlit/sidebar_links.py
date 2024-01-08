80b4746e4f3fab3e3ae0e8b5af0f933acc859a15
Jungrak.Son <91623332+jrson11@users.noreply.github.com>
2024-01-07 18:01:31 -0600

    Update sidebar_links.py

--- streamlit/sidebar_links.py
+++ streamlit/sidebar_links.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 def add_sidebar_links():
-    st.sidebar.title("Links of GeoSohn Plots")
+    st.sidebar.title("Links of GeoSohn")
+    st.sidebar.markdown("[Main](https://geosohn.readthedocs.io/en/latest/)")
     st.sidebar.markdown("[p-y curve](https://plotpycurvepy-j833amvwazrskraswjxgbw.streamlit.app/)")
     st.sidebar.markdown("[q-z curve](https://plotqzcurvepy-2kegu9kpatcrcpwgn3vfas.streamlit.app/)")
     st.sidebar.markdown("[t-z curve](https://plottzcurvepy-mgjf64hfh4dbueqt8rcoez.streamlit.app/)")
