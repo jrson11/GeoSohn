6502082c14ae4a1ea5b40e8a82da0109502a1d42
Jungrak.Son <91623332+jrson11@users.noreply.github.com>
2024-01-10 12:39:32 -0600

    Update MCMC_fake_stock_price.py

--- streamlit/MCMC_fake_stock_price.py
+++ streamlit/MCMC_fake_stock_price.py
@@ -29,7 +29,7 @@ st.subheader(':floppy_disk: Step 1: Import vertical soil data')
 
 #df_Raw = pd.read_csv('./inputs/UW_PCPT_Robertson2010.csv')  
 df_Raw = pd.read_csv('https://raw.githubusercontent.com/jrson11/GeoSohn/main/streamlit/input_MCMC_soil_layers/UW_PCPT_Robertson2010.csv')
-st.write('Imported data: Unit Weight derived from in-situ CPT')
+#st.write('Imported data: Unit Weight derived from in-situ CPT')
 cols = df_Raw.columns   # DataFrame show have two columns, i.e., Z and X
 z_header = cols[0]
 x_header = cols[1]
