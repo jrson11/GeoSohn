1425487bb0897b05d604b47bad62cf6617e591d9
Jungrak.Son <91623332+jrson11@users.noreply.github.com>
2024-01-10 12:38:22 -0600

    Update MCMC_fake_stock_price.py

--- streamlit/MCMC_fake_stock_price.py
+++ streamlit/MCMC_fake_stock_price.py
@@ -13,6 +13,9 @@ import matplotlib.patches as patches
 from scipy.stats import norm
 import streamlit as st
 
+# 서브 스크립트에서 사이드바 링크 추가
+from sidebar_links import add_sidebar_links
+add_sidebar_links()
 
 # Title
 st.title("Probabilistic Stock Market Analysis")
