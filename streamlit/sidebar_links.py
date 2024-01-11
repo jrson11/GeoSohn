615b5f9d2f66a004d88e80784410b7d127597042
Jungrak.Son <91623332+jrson11@users.noreply.github.com>
2024-01-10 20:50:16 -0600

    Update sidebar_links.py

--- streamlit/sidebar_links.py
+++ streamlit/sidebar_links.py
@@ -3,7 +3,8 @@ import streamlit as st
 def add_sidebar_links():
     st.sidebar.title("Links of GeoSohn")
     st.sidebar.markdown("[Main](https://geosohn.readthedocs.io/en/latest/)")
-    st.sidebar.markdown("[MCMC: Stratigraphy](https://mcmcstratigraphypy-ej9ssq7gab64ztknhmyjaw.streamlit.app/)")
+    #st.sidebar.markdown("[MCMC: Stratigraphy](https://mcmcstratigraphypy-ej9ssq7gab64ztknhmyjaw.streamlit.app/)")
+    st.sidebar.markdown("[MCMC: Soil Layers](https://mcmcsoillayerspy-5twn22pprrmtclrnykhwze.streamlit.app/)")
     st.sidebar.markdown("[p-y curve](https://plotpycurvepy-j833amvwazrskraswjxgbw.streamlit.app/)")
     st.sidebar.markdown("[q-z curve](https://plotqzcurvepy-2kegu9kpatcrcpwgn3vfas.streamlit.app/)")
     st.sidebar.markdown("[t-z curve](https://plottzcurvepy-mgjf64hfh4dbueqt8rcoez.streamlit.app/)")
