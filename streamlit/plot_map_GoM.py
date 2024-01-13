d6232e07e9e7ed88cfd4e24aa89fcab9ec3bca1d
Jungrak.Son <91623332+jrson11@users.noreply.github.com>
2024-01-13 17:42:38 -0600

    Update plot_map_GoM.py

--- streamlit/plot_map_GoM.py
+++ streamlit/plot_map_GoM.py
@@ -7,17 +7,23 @@ def create_map():
     # Latitude and longitude coordinates for the center of the Gulf of Mexico
     gulf_of_mexico_coords = [25.8419, -90.4184]
 
-
     # Coordinates for Keathley Canyon Block 293 (example coordinates, replace with actual ones)
     keathley_coords = [26.935, -91.401]  # Replace with actual coordinates
+    # Coordinates for Mississippi Canyon MC 520
+    mississippi_canyon_coords = [28.0, -89.0]  # Replace with actual coordinates
+    # Coordinates for Green Canyon Block 825
+    green_canyon_coords = [27.5, -90.5]  # Replace with actual coordinates
 
     # Create a Folium map centered on the Gulf of Mexico
     folium_map = folium.Map(location=gulf_of_mexico_coords, zoom_start=6)
 
-
     # Add a marker for Keathley Canyon Block 293
     folium.Marker(keathley_coords, popup='Keathley Canyon Block 293').add_to(folium_map)
-    
+    # Add a marker for Mississippi Canyon MC 520
+    folium.Marker(mississippi_canyon_coords, popup='Mississippi Canyon MC 520').add_to(folium_map)
+    # Add a marker for Green Canyon Block 825
+    folium.Marker(green_canyon_coords, popup='Green Canyon Block 825').add_to(folium_map)
+
 
     return folium_map
 
@@ -35,7 +41,7 @@ def main():
     # Button to show the Wikipedia link
     st.markdown('<a href="https://en.wikipedia.org/wiki/Kaskida_Oil_Field" target="_blank">Wiki: Kaskida Oil Field</a>', unsafe_allow_html=True)
     st.markdown('<a href="https://en.wikipedia.org/wiki/Mississippi_Canyon" target="_blank">Wiki: Mississippi Canyon</a>', unsafe_allow_html=True)
-
+    st.markdown('<a href="https://en.wikipedia.org/wiki/Mad_Dog_oil_field" target="_blank">Wiki: Mad Dog oil field</a>', unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
