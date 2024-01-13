44c8191c0ae224e1fafab8e1f9abdf474e60014f
Jungrak.Son <91623332+jrson11@users.noreply.github.com>
2024-01-13 17:48:15 -0600

    Update plot_map_GoM.py

--- streamlit/plot_map_GoM.py
+++ streamlit/plot_map_GoM.py
@@ -10,7 +10,6 @@ def create_map():
     # Create a Folium map centered on the Gulf of Mexico
     folium_map = folium.Map(location=gulf_of_mexico_coords, zoom_start=6)
     
-    '''
     # Coordinates for Keathley Canyon Block 293 (example coordinates, replace with actual ones)
     keathley_coords = [26.935, -91.401]  # Replace with actual coordinates
     # Coordinates for Mississippi Canyon MC 520
@@ -18,15 +17,12 @@ def create_map():
     # Coordinates for Green Canyon Block 825
     green_canyon_coords = [27.5, -90.5]  # Replace with actual coordinates
 
-
-
     # Add a marker for Keathley Canyon Block 293
     folium.Marker(keathley_coords, popup='Keathley Canyon Block 293').add_to(folium_map)
     # Add a marker for Mississippi Canyon MC 520
     folium.Marker(mississippi_canyon_coords, popup='Mississippi Canyon MC 520').add_to(folium_map)
     # Add a marker for Green Canyon Block 825
     folium.Marker(green_canyon_coords, popup='Green Canyon Block 825').add_to(folium_map)
-    '''
 
     return folium_map
 
