297620c88c6e915ff4dd1964e7a573c92fa2b187
J.S <91623332+jrson11@users.noreply.github.com>
2024-01-24 11:02:48 -0600

    Update CPT_processing.py

--- streamlit/CPT_processing.py
+++ streamlit/CPT_processing.py
@@ -12,3 +12,18 @@ import matplotlib.patches as patches
 #from scipy.stats import multivariate_normal as mvn
 #from scipy.stats import norm
 import streamlit as st
+
+# 서브 스크립트에서 사이드바 링크 추가
+from sidebar_links import add_sidebar_links
+add_sidebar_links()
+
+# Title
+st.title("Stratigraphic Soil Layer Modeling (Not Ready)")
+st.write('- Because this runs MCMC everytime when I click any buttons below')
+st.write('- Purpose: Uncertainty quantification')
+st.write('- Method: MCMC calibration based on Bayesian method')
+
+
+# =============================================================================
+# Import raw data
+st.subheader(':floppy_disk: Step 1: Import vertical soil data')
