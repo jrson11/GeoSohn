ebe064c59a32ec4369aa302d63ae59abbfde4b7d
J.S <91623332+jrson11@users.noreply.github.com>
2024-01-24 11:14:50 -0600

    Update CPT_processing.py

--- streamlit/CPT_processing.py
+++ streamlit/CPT_processing.py
@@ -25,9 +25,20 @@ st.write('- Method: Empirical equations based on Robertson2010')
 
 # =============================================================================
 # Import raw data
-st.subheader(':floppy_disk: Step 1: Import vertical soil data')
+st.subheader(':floppy_disk: Step 1: Import in-situ CPT data')
 
 df_Raw = pd.read_csv('https://raw.githubusercontent.com/jrson11/GeoSohn/main/streamlit/input_CPTs_Fugro_TNW/TNW_20200508_FNLM_AGS4.0_V02_F-SCPT_052.csv')
 st.write('Imported data: Unit Weight derived from in-situ CPT')
 
 st.dataframe(df_Raw)
+
+df_CPT = pd.DataFrame()
+df_CPT['LOCA_ID'] = df_Raw.loc[2:,'LOCA_ID']
+df_CPT['Depth_m'] = df_Raw.loc[2:,'SCPT_DPTH']
+df_CPT['qc_MPa'] = df_Raw.loc[2:,'SCPT_RES']
+df_CPT['fs_kPa'] = df_Raw.loc[2:,'SCPT_FRES']
+df_CPT['u2_kPa'] = df_Raw.loc[2:,'SCPT_PWP2']
+df_CPT['qt_MPa'] = df_Raw.loc[2:,'SCPT_QT']
+df_CPT['qnet_MPa'] = df_Raw.loc[2:,'SCPT_QNET']
+
+st.dataframe(df_CPT)
