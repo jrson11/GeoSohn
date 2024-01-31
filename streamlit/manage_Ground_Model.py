d89422836beb0560818165b2f3ec311dd4d555ad
J.S <91623332+jrson11@users.noreply.github.com>
2024-01-31 16:03:17 -0600

    Update manage_Ground_Model.py

--- streamlit/manage_Ground_Model.py
+++ streamlit/manage_Ground_Model.py
@@ -18,9 +18,9 @@ def main():
   list_PC = list(['n/a','b','c'])
   list_JPC = list(['n/a','b','c'])  
   list_CPT = list(['n/a','b','c'])  
-  loca_BC = st.multiselect('Please select the Box Core locations', list_BC,'n/a')
-  loca_PC = st.multiselect('Please select the Piston Core locations', list_PC,'n/a')
-  loca_JPC = st.multiselect('Please select the Jumbo Piston Core locations', list_JPC,'n/a')
+  loca_BC = st.multiselect('Please select the Box Core (**BC**) locations', list_BC,'n/a')
+  loca_PC = st.multiselect('Please select the Piston Core (PC) locations', list_PC,'n/a')
+  loca_JPC = st.multiselect('Please select the Jumbo Piston Core (JPC) locations', list_JPC,'n/a')
   loca_CPT = st.multiselect('Please select the CPT locations', list_CPT,'n/a')
 
 # Membership --------------------------------------------
