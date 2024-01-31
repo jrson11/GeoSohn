3a45a3a381ed5366836509990f9b72cbf9c5578c
J.S <91623332+jrson11@users.noreply.github.com>
2024-01-31 15:53:48 -0600

    Update manage_Ground_Model.py

--- streamlit/manage_Ground_Model.py
+++ streamlit/manage_Ground_Model.py
@@ -4,8 +4,22 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import altair as alt
 
+# =======================================================
+# Confidential
+password = st.sidebar.text_input('Password?', 'password')
+
+# Main --------------------------------------------------
+def main():
+  st.title("Manage Ground Model")
+
+# Membership --------------------------------------------
+if password == st.secrets['DB_pw']:
+  st.markdown('## Welcome to Database by JS')
+  main()
+  
+else:
+  st.markdown('## Sorry, this is confidential')
 
 # Title
-st.title("Manage Ground Model")
 #st.write('- Purpose: To estimate Gmax from CPT data')
 #st.write('- Method: Empirical equations based on Robertson2010')
