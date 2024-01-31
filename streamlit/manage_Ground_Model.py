ed3f015a69b8477a835d383a482f36f70d34aa72
J.S <91623332+jrson11@users.noreply.github.com>
2024-01-31 15:50:18 -0600

    Update manage_Ground_Model.py

--- streamlit/manage_Ground_Model.py
+++ streamlit/manage_Ground_Model.py
@@ -4,3 +4,8 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import altair as alt
 
+
+# Title
+st.title("Manage Ground Model")
+#st.write('- Purpose: To estimate Gmax from CPT data')
+#st.write('- Method: Empirical equations based on Robertson2010')
