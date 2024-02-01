495e5ab9495244518a08ba8b1663ac68a9a7ea77
J.S <91623332+jrson11@users.noreply.github.com>
2024-02-01 08:45:54 -0600

    Update manage_Ground_Model.py

--- streamlit/manage_Ground_Model.py
+++ streamlit/manage_Ground_Model.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
+st.set_page_config(layout="wide")
 import altair as alt
 
 # =======================================================
