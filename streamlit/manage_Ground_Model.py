f5a83792bebaa053925d1a86b0413288fa105811
J.S <91623332+jrson11@users.noreply.github.com>
2024-02-04 16:54:08 -0600

    Update manage_Ground_Model.py

--- streamlit/manage_Ground_Model.py
+++ streamlit/manage_Ground_Model.py
@@ -50,13 +50,14 @@ def map_pyplot(df_LOCA):
   ax.plot(df_LOCA.loc[ii,'LOCA_NATE_ft'],df_LOCA.loc[ii,'LOCA_NATN_ft'],'x',label='CPT')
   
   ## 텍스팅
-  ii1 = df_LOCA['LOCA_TYPE_x'] == 'BC'
-  ii2 = df_LOCA['LOCA_TYPE_x'] == 'PC' 
-  ii3 = df_LOCA['LOCA_TYPE_x'] == 'JPC'
-  ii = df_LOCA['LOCA_TYPE_x'] == any('BC','PC','JPC')
+  ii = df_LOCA['LOCA_TYPE_x'] == 'BC'
+  ax.text(df_LOCA.loc[ii,'LOCA_NATE_ft']+1e2,df_LOCA.loc[ii,'LOCA_NATN_ft'],df_LOCA.loc[ii,'LOCA_ID_x'][-2:], fontsize=8)
+  ii = df_LOCA['LOCA_TYPE_x'] == 'BC'
+  ax.text(df_LOCA.loc[ii,'LOCA_NATE_ft']+1e2,df_LOCA.loc[ii,'LOCA_NATN_ft'],df_LOCA.loc[ii,'LOCA_ID_x'][-2:], fontsize=8)
+  ii = df_LOCA['LOCA_TYPE_x'] == 'BC'
+  ax.text(df_LOCA.loc[ii,'LOCA_NATE_ft']+1e2,df_LOCA.loc[ii,'LOCA_NATN_ft'],df_LOCA.loc[ii,'LOCA_ID_x'][-2:], fontsize=8)
+  ii = df_LOCA['LOCA_TYPE_x'] == 'BC'
   ax.text(df_LOCA.loc[ii,'LOCA_NATE_ft']+1e2,df_LOCA.loc[ii,'LOCA_NATN_ft'],df_LOCA.loc[ii,'LOCA_ID_x'][-3:], fontsize=8)
-  #for i in range(len(df_LOCA)):
-  #  ax.text(df_LOCA.loc[i,'LOCA_NATE_ft']+1e2,df_LOCA.loc[i,'LOCA_NATN_ft'],df_LOCA.loc[i,'LOCA_ID_x'][-3:], fontsize=8)
 
   ## 라벨벨
   ax.set_xlabel("Easting (ft)")
