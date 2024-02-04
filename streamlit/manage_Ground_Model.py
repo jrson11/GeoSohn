14a2a603539d637bdcd59936e45b71f2cdee9b9f
J.S <91623332+jrson11@users.noreply.github.com>
2024-02-04 16:03:42 -0600

    Update manage_Ground_Model.py

--- streamlit/manage_Ground_Model.py
+++ streamlit/manage_Ground_Model.py
@@ -107,7 +107,7 @@ def plot_CPT(df_SCPT):
   for i in range(n_loca):
     loca_name = loca_list[i]
     ii = loca_name == df_SCPT['LOCA_ID']
-      ax[0].plot(df_SCPT.loc[ii,'SCPT_QNET_ksf'],df_SCPT.loc[ii,'SCPT_DPTH_ft'], '.', label=loca_name)
+    ax[0].plot(df_SCPT.loc[ii,'SCPT_QNET_ksf'],df_SCPT.loc[ii,'SCPT_DPTH_ft'], '.', label=loca_name)
   ax[0].set_ylabel('Depth (ft)')
   ax[0].set_xlabel('qnet (ksf)')
   ax[0].set_ylim([zmax_ft,0])
