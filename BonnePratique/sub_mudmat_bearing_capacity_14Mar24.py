8569698cfdc705f750318922e7ca9d877c6f5ac6
J.S <91623332+jrson11@users.noreply.github.com>
2024-03-14 15:40:12 -0500

    Update sub_mudmat_bearing_capacity_14Mar24.py

--- BonnePratique/sub_mudmat_bearing_capacity_14Mar24.py
+++ BonnePratique/sub_mudmat_bearing_capacity_14Mar24.py
@@ -18,7 +18,7 @@ def clay_bearing_capacity(project):
     st.sidebar.subheader('Special thanks to the my Advisor Philippe Jeanjean, Ph.D., P.E., F.ASCE')
     st.sidebar.write(':blue[Purpose]: To estimate Factor of Safety from offshore mudmat bearing capacity analysis with CLAY soils.')
     st.sidebar.write(':blue[How to use]: Left columns has three tabs. Please fill out input data in the 1st tab.')
-    st.sidebar.write(':blue[Last update]: 03/14/2024 by Jung')
+    st.sidebar.write(':blue[Last update]: 03/14/2024')
     
     col1, col2 = st.columns([1,2])
 
