32e9c21e0e9f44710a08fffe29dd139410e37c62
J.S <91623332+jrson11@users.noreply.github.com>
2024-03-14 13:21:37 -0500

    Update sub_clay_bearing_capacity_14Mar24.py

--- BonnePratique/sub_clay_bearing_capacity_14Mar24.py
+++ BonnePratique/sub_clay_bearing_capacity_14Mar24.py
@@ -31,7 +31,7 @@ def clay_bearing_capacity(project):
             with col_g2:
                 L = st.number_input('L: Length (m)', value = 10)      # Length (m)
             with col_g3:
-                D = st.number_input('D\nEmbedment (m) ', value = 2)       # Embedment (m)
+                D = st.number_input('D \n Embedment (m) ', value = 2)       # Embedment (m)
 
             ## Loads
             st.subheader('Loads')
