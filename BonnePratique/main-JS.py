ce1aea879ee0371a7d179724dc1899135021620d
J.S <91623332+jrson11@users.noreply.github.com>
2024-03-14 16:11:55 -0500

    Update main-JS.py

--- BonnePratique/main-JS.py
+++ BonnePratique/main-JS.py
@@ -30,7 +30,7 @@ def bp_mudmat_bearing_capacity(project):
   #from sub_mudmat_bearing_capacity_14Mar24_v2 import clay_bearing_capacity
   #clay_bearing_capacity(project)
 
-  from sub_mudmat_bearing_capacity_14Mar24_v2 import Dog
+  import sub_mudmat_bearing_capacity_14Mar24_v2 
   dog1 = Dog("Fido")
   dog2 = Dog("Rover")
 
