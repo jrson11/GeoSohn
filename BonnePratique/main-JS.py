f9529c17511932fb89a23ce3f4e04897e022375f
J.S <91623332+jrson11@users.noreply.github.com>
2024-03-14 16:33:38 -0500

    Update main-JS.py

--- BonnePratique/main-JS.py
+++ BonnePratique/main-JS.py
@@ -32,8 +32,8 @@ def bp_mudmat_bearing_capacity(project):
     from sub_mudmat_bearing_capacity_14Mar24_v2 import eg_clay_bearing_capacity
     eg_clay_bearing_capacity()
   else:
-    from sub_mudmat_bearing_capacity_14Mar24_v2 import clay_bearing_capacity
-    clay_bearing_capacity(project)
+    from sub_mudmat_bearing_capacity_14Mar24_v2 import inputs_for_mudmat
+    inputs_for_mudmat(project)
 
 def bp_mudmat_settlement(project):
   st.header(project)
