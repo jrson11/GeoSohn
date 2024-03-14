947e0a28a69d6eb47b191b250a4b5d043a9133dc
J.S <91623332+jrson11@users.noreply.github.com>
2024-03-14 13:29:24 -0500

    Update sub_clay_bearing_capacity_14Mar24.py

--- BonnePratique/sub_clay_bearing_capacity_14Mar24.py
+++ BonnePratique/sub_clay_bearing_capacity_14Mar24.py
@@ -43,8 +43,8 @@ def clay_bearing_capacity(project):
                 Hext = st.number_input('Hext: External H load (kN)', value = 500)   # External Horizontal load (kN)
                 θ = st.number_input('θ: angle b/w Hext & L-axis (deg))', value = 0)           # angle between Hext and long axis (deg)
             with col_l3:
-                Mext_B = st.number_input('Mext_B: Moment in B-axis', value = 0) # Moment in B direction (kNm)
-                Mext_L = st.number_input('Mext_L: Moment in L-axis', value = 0) # Moment in L direction (kNm)
+                Mext_B = st.number_input('Mext_B: Moment in B-axis (kNm)', value = 0) # Moment in B direction (kNm)
+                Mext_L = st.number_input('Mext_L: Moment in L-axis (kNm)', value = 0) # Moment in L direction (kNm)
 
             ## Soil
             st.subheader('Soil')
