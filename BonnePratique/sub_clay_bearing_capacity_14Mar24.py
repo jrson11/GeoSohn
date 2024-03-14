8daca33f0a9c80a2c9e146e2b6e3782cfdae9921
J.S <91623332+jrson11@users.noreply.github.com>
2024-03-14 13:33:12 -0500

    Update sub_clay_bearing_capacity_14Mar24.py

--- BonnePratique/sub_clay_bearing_capacity_14Mar24.py
+++ BonnePratique/sub_clay_bearing_capacity_14Mar24.py
@@ -28,12 +28,15 @@ def clay_bearing_capacity(project):
             st.subheader('Foundation Geometry')
             col_g1,col_g2,col_g3 = st.columns(3)
             with col_g1:
-                B = st.number_input('B', value = 10)      
-                st.write(':blue[B]: Width (m)')
+                B = st.number_input('B', value = 10)
             with col_g2:
                 L = st.number_input('L', value = 10)      # Length (m)
             with col_g3:
                 D = st.number_input('D', value = 2)       # Embedment (m)
+            if onComments:
+                st.write(':blue[B]: Width (m)')
+                st.write(':blue[L]: Length (m)')
+                st.write(':blue[D]: Embedment (m)')
 
             ## Loads
             st.subheader('Loads')
