369d3436b4401038457190f6279a3ae4b591eb94
J.S <91623332+jrson11@users.noreply.github.com>
2024-03-14 20:45:39 -0500

    Update main-JS.py

--- BonnePratique/main-JS.py
+++ BonnePratique/main-JS.py
@@ -16,15 +16,18 @@ st.set_page_config(layout="wide")
 # 비번 확인
 password = st.sidebar.text_input('Password?')
 
-# Add custom CSS to hide the GitHub icon
-hide_github_icon = """
-#MainMenu {
-  visibility: hidden;
-}
-"""
-
-
-st.markdown(hide_github_icon, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 
 # ====================================================================
